676c8454cdc20169b7017a155c11b1c1a45ea76d
Anna Detskaya <a.detskaya@fin.by>
2026-09-21 18:40:35 +0300

    Configure incremental merge for flights staging model

--- models/staging/flights/stg_flights__flights.sql
+++ models/staging/flights/stg_flights__flights.sql
@@ -1,6 +1,8 @@
 {{
     config(
-        materialized = 'table'
+        materialized = 'incremental',
+        incremental_strategy = 'merge',
+        unique_key = ['flight_id']
     )
 }}
 
@@ -8,3 +10,12 @@ select
     flight_id, flight_no, scheduled_departure, scheduled_arrival, departure_airport, arrival_airport, status, aircraft_code, actual_departure, actual_arrival 
 from 
     {{ source('demo_src', 'flights') }}
+
+{% if is_incremental() %}
+    where 
+        scheduled_departure >= (
+            select max(scheduled_departure) - interval '100 day'
+            from {{ source('demo_src', 'flights') }}
+        )
+
+{% endif %}
